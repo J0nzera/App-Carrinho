ae499655ea83b9c9deeb54ab4a423bd00f395a1e
Jon Silva <jonathasinv@gmail.com>
2020-04-14 19:03:46 -0300

    set dados

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.room.Room;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.room.Room;
 
 import com.example.myapplication.bd.AppDatabase;
 import com.example.myapplication.bd.Produto;
@@ -12,31 +16,36 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    AppDatabase db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bancoDb();
+        AppDatabase db = Room.databaseBuilder(getApplication(),
+                AppDatabase.class, "database-name").build();
+
     }
 
-    public void bancoDb() {
+   public void carregar (View view) {
         new Thread() {
 
             @Override
-            public void run() {
-
-                AppDatabase db = Room.databaseBuilder(getApplication(),
-                        AppDatabase.class, "database-name").build();
+                    public void run () {
 
                 List<Produto> produtos = db.produtoDao().getAll();
-                Produto produto = produtos.get(0);
-                System.out.println(produto.nomeprod);
-                System.out.println(produto.descprod);
-                System.out.println(produto.valorprod);
+                TextView textView = findViewById(R.id.ttnome);
+                TextView textView1 = findViewById(R.id.ttdesc);
+                TextView textView2 = findViewById(R.id.ttvalor);
 
-                }
+                Produto produto = produtos.get(0);
+                textView.setText(produto.nomeprod);
+                textView1.setText(produto.descprod);
+                textView2.setText(produto.valorprod);
+
+            }
         }.start();
-    }
+   }
 
 }
