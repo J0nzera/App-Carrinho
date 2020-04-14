301bad3da4b8a21209d2e092b34a3d3654a58a30
Jon Silva <jonathasinv@gmail.com>
2020-04-14 18:03:07 -0300

    add dados

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 import android.os.Bundle;
+
+import com.example.myapplication.bd.AppDatabase;
+import com.example.myapplication.bd.Produto;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bancoDb();
     }
+
+    public void bancoDb() {
+        new Thread() {
+
+            @Override
+            public void run() {
+
+                AppDatabase db = Room.databaseBuilder(getApplication(),
+                        AppDatabase.class, "database-name").build();
+
+                Produto produto = new Produto();
+                produto.nomeprod = "Playstation";
+                produto.descprod = "Descrição do Produto";
+                produto.valorprod = "999,999";
+
+                db.produtoDao().insertAll(produto);
+
+                }
+        }.start();
+    }
+
 }
